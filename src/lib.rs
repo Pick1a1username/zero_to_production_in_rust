297c6eb4dbcc5f146eb2df295ee992602475048c
Pick1a1username <xscreensaver@outlook.com>
2023-08-07 21:40:01 +0900

    new endpoint /subscriptions added.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,18 @@ use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use actix_web::dev::Server;
 use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
+
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
@@ -10,6 +21,9 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
-    }) .listen(listener)? .run();
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
