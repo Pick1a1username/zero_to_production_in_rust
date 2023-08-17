692e839884280e0e280d9a2b7257ea24c90cf559
Pick1a1username <xscreensaver@outlook.com>
2023-08-17 21:39:16 +0900

    configuration loaded from a yaml file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use zero_to_production_in_rust::startup::run;
+use zero_to_production_in_rust::configuration::get_configuration;
 
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000")
-        .expect("Failed to bind random port");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
