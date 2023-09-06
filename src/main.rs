840636a8926a79415599cbfb94436b309e4beb0b
Pick1a1username <xscreensaver@outlook.com>
2023-09-06 22:21:22 +0900

    logs improved and refactored

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,19 @@
 use zero_to_production_in_rust::startup::run;
 use zero_to_production_in_rust::configuration::get_configuration;
+use zero_to_production_in_rust::telemetry::{get_subscriber, init_subscriber};
 
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    LogTracer::init().expect("Failed to set logger");
-
-    let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "zero_to_production_in_rust".into(),
+    let subscriber = get_subscriber(
+        "zero2prod".into(),
+        "info".into(),
         std::io::stdout
     );
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(
         &configuration.database.connection_string() )
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,9 +1,32 @@
+use once_cell::sync::Lazy;
 use std::net::TcpListener;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 
 use zero_to_production_in_rust::configuration::{get_configuration, DatabaseSettings};
 use zero_to_production_in_rust::startup::run;
+use zero_to_production_in_rust::telemetry::{get_subscriber, init_subscriber};
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(
+            subscriber_name,
+            default_filter_level,
+            std::io::stdout
+        );
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(
+            subscriber_name,
+            default_filter_level,
+            std::io::sink
+        );
+        init_subscriber(subscriber);
+    };
+});
 
 pub struct TestApp {
     pub address: String,
@@ -31,6 +54,8 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool { // Create
 }
 
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
